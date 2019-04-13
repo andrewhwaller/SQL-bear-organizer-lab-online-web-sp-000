81ba49179068b68591687cc4aaf2d5fe6a5b6bcb
Andrew Waller <andrewhwaller@gmail.com>
2019-04-13 04:18:04 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears VALUES (4, 'Sergeant Brown', 27, 'male', 'brown', 'stern', 1);
 INSERT INTO bears VALUES (6, 'Melissa', 24, 'female', 'brown', 'happy', 1);
 INSERT INTO bears VALUES (6, 'Grinch', 30, 'male', 'brown', 'bitter', 1);
 INSERT INTO bears VALUES (7, 'Wendy', 21, 'female', 'brown', 'happy', 1);
-INSERT INTO bears VALUES (8, NULL, 22, 'male', 'brown', 'unknown', 1);
+INSERT INTO bears VALUES (8, null, 22, 'male', 'brown', 'unknown', 1);
