2e9af7431838cc4a3d39c41a00e4965c53bc714d
Andrew Waller <andrewhwaller@gmail.com>
2019-04-13 04:06:42 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -15,4 +15,4 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Se
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Melissa', 24, 'female', 'brown', 'happy', true);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Grinch', 30, 'male', 'brown', 'bitter', true);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Wendy', 24, 'female', 'brown', 'happy', true);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('NULL', 22, 'male', 'brown', 'unknown', true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (' ', 22, 'male', 'brown', 'unknown', true);
