5a83ddb04eb52a6a7125830b2982c8132e083d5a
Andrew Waller <andrewhwaller@gmail.com>
2019-04-13 04:27:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES (1, 'Mr. Chocolate', 10, 'm', 'brown', 'happy', 1);
-INSERT INTO bears VALUES (2, 'Rowdy', 23, 'm', 'brown', 'rowdy', 1);
-INSERT INTO bears VALUES (3, 'Tabitha', 22, 'f', 'brown', 'neutral', 1);
-INSERT INTO bears VALUES (4, 'Sergeant Brown', 27, 'm', 'brown', 'stern', 1);
-INSERT INTO bears VALUES (6, 'Melissa', 24, 'f', 'brown', 'happy', 1);
-INSERT INTO bears VALUES (6, 'Grinch', 30, 'm', 'brown', 'bitter', 1);
-INSERT INTO bears VALUES (7, 'Wendy', 21, 'f', 'brown', 'happy', 1);
-INSERT INTO bears VALUES (8, null, 22, 'm', 'brown', 'unknown', 1);
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 10, 'male', 'brown', 'happy', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 23, 'male', 'brown', 'rowdy', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 22, 'female', 'brown', 'neutral', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 27, 'male', 'brown', 'stern', 1);
+INSERT INTO bears VALUES (6, 'Melissa', 24, 'female', 'brown', 'happy', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 30, 'male', 'brown', 'bitter', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 21, 'female', 'brown', 'happy', 1);
+INSERT INTO bears VALUES (8, null, 22, 'male', 'brown', 'unknown', 1);
